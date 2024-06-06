c545e3f9595f64f897f8e53042ab3c48eac2d01e
Flament Julien <julien.flmt@gmail.com>
2024-06-06 09:29:55 +0200

    Corrections mineures

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -22,29 +22,27 @@ class ContactController extends AbstractController
         if($form->isSubmitted() && $form->isValid()) {
             $data = $form->getData();
 
-            $adr = $data['email'];
-            $content = $data['Contenu'];
             $email = (new Email())
-            ->from($adr)
+            ->from($data['email'])
             ->to('julien.flmt@gmail.com')
             //->cc('cc@example.com')
             //->bcc('bcc@example.com')
             //->replyTo('fabien@example.com')
             //->priority(Email::PRIORITY_HIGH)
             ->subject('Demande de support')
-            ->text($content);
+            ->text($data['Contenu']);
             //->html('<p>See Twig integration for better HTML integration!</p>');
 
         $mailer->send($email);
 
         $this->addFlash('success', 'Votre email a été envoyé avec succès.');
 
-        return $this->redirectToRoute('app_home');
+        return $this->redirectToRoute('app_contact');
         }
 
         return $this->render('contact/index.html.twig', [
             'controller_name' => 'ContactController',
-            'formulaire' => $form
+            'formulaire' => $form->createView()
         ]);
     }
 }
---
+++ src/Form/UserProfileFromType.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserProfileFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('username', TextType::class, [
+                'label' => "Nom d'utilisateur",
+                'attr' => ['class' => 'form-control']
+            ])
+            ->add('plainPassword', PasswordType::class, [
+                'label' => 'Nouveau mot de passe',
+                'required' => false,
+                'mapped' => false,
+                'attr' => ['class' => 'form-control']
+            ])
+            ->add('save', SubmitType::class, [
+                'label' => 'Enregistrer les modifications',
+                'attr' => ['class' => 'btn btn-primary']
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            // Configure your form options here
+        ]);
+    }
+}
